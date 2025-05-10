40142d88f8499fb96eb1e2e191a9db947e88a90b
MonaLisa <monalisa@mojababa.com>
2025-05-10 23:58:40 +0200

    Added /order route and changed structure of a project

---
+++ shop/customer.py
@@ -0,0 +1,77 @@
+from flask import Flask, render_template, request, jsonify
+
+from models.models import database
+
+from utilities.utilities import ErrorHandler
+
+from utilities.databaseUtils import check_product_on_id, get_all_products, add_order,add_order_product,search_categories_on_name, search_products_on_name
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+
+database.init_app(app)
+
+
+#TODO: Should do the transition to base.html so it looks better and easier
+@app.route("/search",methods=["GET","POST"])
+def search():
+    if request.method == "POST":
+        product_name = request.form.get("pname")
+        search_product = "%{}%".format(product_name)
+        category_name = request.form.get("category")
+        search_category = "%{}%".format(category_name)
+
+        categories = search_categories_on_name(search_category,search_product)
+        products = search_products_on_name(search_category, search_product)
+        json_products = []
+        for product in products:
+            json_products.append({"categories":[category.name for category in product.categories],"id":product.id,"name":product.name,"price":product.price})
+
+        return jsonify({"categories":[category.name for category in categories],"products":[product for product in json_products]}),200
+
+    return render_template("search.html")
+
+
+@app.route("/order",methods=["GET","POST"])
+def order():
+    products = get_all_products()
+    if request.method == "POST":
+        product_ids = request.form.getlist('productsId')
+        product_quants = request.form.getlist('productsQuant')
+        sending_req = []
+        try:
+            for product_id, product_quant in zip(product_ids,product_quants):
+                if int(product_quant) == 0:
+                    continue
+                if int(product_quant) < 0:
+                    raise ErrorHandler("Invalid product quantity.",400)
+                
+                product_exist = check_product_on_id(product_id)
+                if not product_exist:
+                    raise ErrorHandler("Invalid product id.",400)
+                
+                sending_req.append({"id":product_id,"quantity":product_quant})
+            
+            if sending_req == []:
+                raise ErrorHandler("Please pick a product to order.",400)
+
+        except ErrorHandler as e:
+            return jsonify({"message":e.message}),e.error_code
+        
+        order = add_order()
+        database.session.commit()
+
+        for x in sending_req:
+            add_order_product(order.id,int(x['id']), int(x['quantity']))
+
+        database.session.commit()
+
+        return jsonify({"id":order.id}),200
+
+    return render_template("order.html", products=products)
+
+
+if __name__=="__main__":
+    app.run(debug=True,port=5200)
--- shop/models/models.py
+++ shop/models/models.py
@@ -10,12 +10,15 @@ product_category = database.Table(
 )
 
 
+
 class Product(database.Model):
     __tablename__='products' 
     id = database.Column(database.Integer, primary_key=True, autoincrement=True)
     name = database.Column(database.String(64), nullable=False)
     price = database.Column(database.Double, nullable=False)
     categories = database.relationship('Category', secondary=product_category, backref='products')
+
+    orders = database.relationship('OrderProduct',back_populates='product')
 
 
     def __init__(self,name,price):
@@ -27,6 +30,33 @@ class Category(database.Model):
     __tablename__='categories'
     id = database.Column(database.Integer, primary_key=True, autoincrement=True)
     name = database.Column(database.String(64), nullable=False)
-
+    
     def __init__(self,name):
         self.name = name
+
+class Order(database.Model):
+    __tablename__ = "orders"
+    id = database.Column(database.Integer, primary_key=True, autoincrement=True)
+    #Probably will add email of a user
+    products = database.relationship("OrderProduct", back_populates='order')
+
+class OrderProduct(database.Model):
+    __tablename__='order_product'
+    order_id = database.Column(database.Integer,database.ForeignKey('orders.id'),primary_key=True)
+    product_id = database.Column(database.Integer,database.ForeignKey('products.id'),primary_key=True)
+
+    quantity = database.Column(database.Integer, nullable=False)
+
+
+    order = database.relationship('Order', back_populates='products')
+    product = database.relationship('Product', back_populates='orders')
+
+    def __init__(self,order_id,product_id,quantity):
+        self.product_id = product_id
+        self.order_id = order_id
+        self.quantity = quantity
+
+
+
+
+
